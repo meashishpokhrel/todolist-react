8f489826a08f881c08fbdb1394a59cc15b128a4c
meashishpokhrel <meashishpokhrel@gmail.com>
2020-05-29 12:51:19 +0545

    state added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import Todos from "./components/Todos.component";
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id:1,
+        title: "Lets do this!",
+        compeleted: false
+      },
+      {
+        id:2,
+        title: "Do homework",
+        compeleted: false
+      },
+      {
+        id:3,
+        title: "Covid 19 tracker app",
+        compeleted: false
+      }
+    ]
+  }
   render(){
+    console.log(this.state.todos);
     return (
       <div className="App">
         <h1>React</h1>
