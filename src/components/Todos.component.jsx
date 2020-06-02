42eab9f7f91ef3766b3b2ae26aaea50f46efe0c9
meashishpokhrel <meashishpokhrel@gmail.com>
2020-06-02 21:38:24 +0545

    ui to list added

--- src/components/Todos.component.jsx
+++ src/components/Todos.component.jsx
@@ -2,13 +2,9 @@ import React, {Component} from 'react';
 
 class Todos extends Component {
   render(){
-    console.log(this.props.todos);
-
-    return (
-      <div className="App">
-        <h1>This is todolist</h1>
-      </div>
-    );
+    return this.props.todos.map((todo) => (
+    <h3> { todo.title } </h3>
+    ));
   }
 }
 
