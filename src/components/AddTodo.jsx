6c86269f1ec98027cdf18089e82cffdcadf79d7d
meashishpokhrel <meashishpokhrel@gmail.com>
2020-06-04 13:28:53 +0545

    input text taken through event

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 
 export class AddTodo extends Component {
+    state = {
+        title: ""
+    }
+
+    onChange = (e) => {
+        this.setState( {[e.target.name]: e.target.value});
+    }
     render() {
         return (
             <form style={{display:"flex"}}>
@@ -8,6 +15,8 @@ export class AddTodo extends Component {
                  name="title" 
                  placeholder="Add todo ..."
                  style={{flex: "10", padding: "5px"}}
+                 value= {this.state.title}
+                 onChange={this.onChange}
                 />
 
                 <input type="submit" 
