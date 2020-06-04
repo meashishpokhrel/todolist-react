b9348459eb62ac81775e2e75ad5b306ee821b1f8
meashishpokhrel <meashishpokhrel@gmail.com>
2020-06-04 15:19:45 +0545

    Home and About Route UI added

--- src/components/layouts/header.jsx
+++ src/components/layouts/header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Header() {
     return(
-        <h1 style={titleStyle}>Todo List </h1>
-        
+        <header style={titleStyle}>
+        <h1 >Todo List </h1>
+        <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
+        </header>
     )
 }
 
@@ -12,5 +15,10 @@ const titleStyle = {
     color: "#fff",
     textAlign: "center",
     padding: "10px"
+}
+
+const linkStyle = {
+    color: "white",
+    textDecoration: "none"
 }
 export default Header;
