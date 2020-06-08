e462a846c307be75b07704789767e7bdb5511d61
meashishpokhrel <meashishpokhrel@gmail.com>
2020-06-09 00:04:42 +0545

    Updates

---
+++ precache-manifest.cc091fe1276c0362d9da6d7dff48fb39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a3749be25c6d32426b0d226da59e13",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "233d7559d310e18cf2b6",
+    "url": "/todolist-react/static/css/main.9cc2c256.chunk.css"
+  },
+  {
+    "revision": "30064c3f57c427e3b64e",
+    "url": "/todolist-react/static/js/2.e7c1af99.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todolist-react/static/js/2.e7c1af99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233d7559d310e18cf2b6",
+    "url": "/todolist-react/static/js/main.7ae81ff0.chunk.js"
+  },
+  {
+    "revision": "40f67da1a4107c218fc9",
+    "url": "/todolist-react/static/js/runtime-main.b856cca8.js"
+  }
+]);
